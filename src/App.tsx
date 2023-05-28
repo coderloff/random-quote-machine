d314cdac6a6fa0e462ab62110834e8c5cde67b62
Coderloff <coderloff@gmail.com>
2023-05-28 19:10:37 +0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,22 @@ const quoteData = [
     text: `Life is what happens when you’re busy making other plans.`,
     author: "John Lennon",
   },
+  {
+    text: `The greatest glory in living lies not in never falling, but in rising every time we fall.`,
+    author: "Nelson Mandela",
+  },
+  {
+    text: `You will face many defeats in life, but never let yourself be defeated.`,
+    author: "Maya Angelou",
+  },
+  {
+    text: `Never let the fear of striking out keep you from playing the game.`,
+    author: "Babe Ruth",
+  },
+  {
+    text: `Life is either a daring adventure or nothing at all.`,
+    author: "Helen Keller",
+  },
   { text: `Get busy living or get busy dying.`, author: "Stephen King" },
   {
     text: `You only live once, but if you do it right, once is enough.`,
@@ -25,7 +41,7 @@ const colorData = [
   { color: "rgb(243, 156, 18)" },
   { color: "rgb(251, 105, 100)" },
   { color: "rgb(155, 89, 182)" },
-  { color : "rgb(119, 177, 169)" },
+  { color: "rgb(119, 177, 169)" },
   { color: "rgb(231, 76, 60)" },
   { color: "rgb(189, 187, 153)" },
   { color: "rgb(115, 168, 87)" },
@@ -33,10 +49,10 @@ const colorData = [
 
 console.log(quoteData);
 const getRandomQuote = () => {
-  return Math.round(Math.random() * (quoteData.length - 1 - 0) + 0);
+  return Math.round(Math.random() * (quoteData.length - 1));
 };
 const getRandomColor = () => {
-  return Math.round(Math.random() * (colorData.length - 1 - 0) + 0);
+  return Math.round(Math.random() * (colorData.length - 1));
 };
 
 const App = () => {
@@ -49,7 +65,7 @@ const App = () => {
   };
   return (
     <main>
-      <Quote handleNewQuote={handleNewQuote} quote={quote} color={color}/>
+      <Quote handleNewQuote={handleNewQuote} quote={quote} color={color} />
       <div className="attributes">
         <span className="text">by Coderloff</span>
       </div>
